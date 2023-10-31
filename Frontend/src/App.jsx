4df8a1a02ae15132b9b6a6ce86cb33019efc9e3b
Mariya B <supermariyabros@gmail.com>
2023-10-31 16:56:26 +0300

    🟥 Bug Fix

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,13 @@
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
-import { Routes, Route, Switch } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 
 const App = () => {
   return (
     <Routes>
-      <Switch>
       <Route path="/login" Component={Login} />
       <Route path="/register" Component={Register} />
-      </Switch>
     </Routes>
   )
 }
